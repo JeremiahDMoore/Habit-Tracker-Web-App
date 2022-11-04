db2c01996d70545692882d09b82577d0367874da
Jeremiah Moore <jdmenterprises@gmail.com>
2022-11-04 01:36:22 -0700

    changed code to actively listen on open port

--- server.js
+++ server.js
@@ -9,18 +9,27 @@ const logger = require('morgan')
 const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
 const todoRoutes = require('./routes/todos')
-
+// use .ENV file for environment variables
 require('dotenv').config({path: './config/.env'})
+//new stuff
+const { MongoClient } = require('mongodb');
+const uri = process.env.DB_STRING;
+const client = new MongoClient(uri);
+//
+
 
 // Passport config
 require('./config/passport')(passport)
-
+// Connect to MongoDB
 connectDB()
-
+// EJS in views folder
 app.set('view engine', 'ejs')
+// static folder
 app.use(express.static('public'))
+// body parser
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+// logging
 app.use(logger('dev'))
 // Sessions
 app.use(
@@ -35,12 +44,35 @@ app.use(
 // Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
-
+// use flash messages for errors, etc
 app.use(flash())
-  
+ // set up routes for which the server is listening 
 app.use('/', mainRoutes)
 app.use('/todos', todoRoutes)
- 
-app.listen(process.env.PORT, ()=>{
-    console.log('Server is running on 2122, you better catch it!')
-})    
+
+// OG code
+// app.listen(process.env.PORT, ()=>{
+//     console.log('Server is running on 2122, you better catch it!')
+// })
+//new stuff
+const dbS = connectDB().then(() => {
+
+
+  client.connect(async err => {
+    //Connect To Database
+    // connectDB();
+    // if(!connect) {console.log('Mongoose ran away'); return false}
+    if(err){ console.error(err); return false;}
+    // connection to mongo is successful, listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("listening for requests");
+    })
+  });
+  // connectDB(app)
+  //
+  
+  })
+
+
+
+
